c8b5a493e0064adf8038621994c7f8d7cd009848
D27-Lab <Lavrik98@inbox.lv>
2025-07-31 14:28:56 +0300

    Добавлен калькулятор с методами

---
+++ src/Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Ошибка: деление на ноль невозможно.");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + " + " + num2 + " = " + add(num1, num2));
+        System.out.println(num1 + " - " + num2 + " = " + subtract(num1, num2));
+        System.out.println(num1 + " * " + num2 + " = " + multiply(num1, num2));
+        System.out.println(num1 + " / " + num2 + " = " + divide(num1, num2));
+    }
+}
